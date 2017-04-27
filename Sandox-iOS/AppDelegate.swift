4146f4c01bd455963885419d00c78c728ca41cf3
Chenxia Liu <liuche@mozilla.com>
2017-04-27 13:47:42 -0700

    Fully remove storyboards for WebView.

--- Sandox-iOS/AppDelegate.swift
+++ Sandox-iOS/AppDelegate.swift
@@ -9,7 +9,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
         window?.rootViewController = ViewController()
